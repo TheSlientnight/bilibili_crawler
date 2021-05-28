6a2c57c42092b730ffd0a179c313918e3490744f
QuinnChang <quinnchang@zhuanxinbaoxian.com>
2021-05-28 18:24:52 +0800

    get comments

--- common/barrage_crawler.py
+++ common/barrage_crawler.py
@@ -2,6 +2,8 @@ import string
 import time
 import re
 import math
+import threading
+import threadpool
 
 import bs4
 import jieba
@@ -17,18 +19,25 @@ logger.add("../{}.log".format(time.strftime('%Y-%m-%d')))
 
 NAME = ""
 
+lock = threading.Lock()
+pool = threadpool.ThreadPool(10)
+
 
 def get_av(url):
     """
     :param url:视频地址
     :return: 视频av号
     """
-    p = r"(?<=aid\":)\d*"
+    global NAME
+    p_av = r"(?<=aid\":)\d*"
+    p_name = r"(?<=content=\")([^\x00-\xff]*[\d]*[\w]*)(?=_哔哩哔哩)"
     for i in range(3):
         try:
             res = requests.get(url).text
             logger.debug("正在提取视频av号")
-            aid = re.search(p, res).group()
+            aid = re.search(p_av, res).group()
+            NAME = re.search(p_name, res).group()
+            logger.debug("视频名称:{}, av号:{}".format(NAME, aid))
             return aid
         except Exception as e:
             logger.error("请求{}失败,失败原因:{}".format(url, e))
@@ -40,7 +49,6 @@ def get_cid(url):
     logger.debug("正在提取视频的bvid")
     bvid = re.search(p, url).group()
     barrage_url = ""
-    global NAME
     # 重试三次
     for i in range(3):
         try:
@@ -48,7 +56,6 @@ def get_cid(url):
             res = requests.get("https://api.bilibili.com/x/player/pagelist?bvid={}&jsonp=jsonp".format(bvid)).json()
             cid = jsonpath(res, "$..data[0]")[0]['cid']
             barrage_url = f"https://comment.bilibili.com/{cid}.xml"
-            NAME = cid
             return barrage_url
         except Exception as e:
             logger.error("获取cid失败,错误原因:{}".format(e))
@@ -60,76 +67,71 @@ def get_cid(url):
 
 
 # 获取弹幕
-def get_barrage(barrage_url) -> str:
+def get_barrage(viedo_url) -> str:
     """
-    :param barrage_url:视频地址
+    :param viedo_url:视频地址
     :return:爬取结果的xml文件
     """
+    barrage_url = get_cid(viedo_url)
     HEADERS = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
                       "Chrome/90.0.4430.212 Safari/537.36 Edg/90.0.818.66 "
     }
     for i in range(3):
         try:
-            logger.info(f"正在爬取{barrage_url}的弹幕")
+            logger.info(f"正在爬取{viedo_url}的弹幕")
             res = requests.get(barrage_url, headers=HEADERS).content.decode("utf-8")
             return res
         except Exception as e:
-            logger.error(f"爬取{barrage_url}的弹幕失败，错误原因:{e}")
+            logger.error(f"爬取{viedo_url}的弹幕失败，错误原因:{e}")
             continue
 
 
 # 爬取评论
-def get_comment(url):
-    aid = get_av(url)
+def get_comments(aid) -> list:
     comment_list = []
+
+    def get_comment(page):
+        res = requests.get("https://api.bilibili.com/x/v2/reply?pn={}&type=1&oid={}&sort=1"
+                           .format(page, aid)).json()
+        comment = jsonpath(res, r"$..data[replies]..[content][message]")
+        lock.acquire()
+        comment_list.extend(comment)
+        lock.release()
+
     for i in range(3):
         try:
-            res = requests.get("https://api.bilibili.com/x/v2/reply?pn=1&type=1&oid={}&sort=1".format(aid)).json()
-            comment_count = jsonpath(res, "$..data[page].count")[0]
-            for i in range(math.ceil(comment_count / 20)):
-                res = requests.get("https://api.bilibili.com/x/v2/reply?pn={}&type=1&oid={}&sort=1".format(i + 1, aid)).json()
-                comment = jsonpath(res, r"$..data[replies]..[content][message]")
-                comment_list.extend(comment)
-                return comment_list
+            num_res = requests.get("https://api.bilibili.com/x/v2/reply?pn=1&type=1&oid={}&sort=1".format(aid)).json()
+            comment_count = jsonpath(num_res, "$..data[page].count")[0]
+            page_num = math.ceil(comment_count / 20)
+            logger.debug(f"共计{page_num}页")
+            pg_list = [pg for pg in range(1, page_num)]
+            tasks = threadpool.makeRequests(get_comment, pg_list)
+            [pool.putRequest(task) for task in tasks]
+            pool.wait()
         except Exception as e:
             logger.error("爬取评论时发生错误,错误详情:{}".format(e))
+            continue
+        if comment_list:
+            return comment_list
+        else:
+            logger.error("爬取评论时发生错误,任务终止")
+            break
 
 
-# 处理爬取结果
-def parse_html(text) -> list:
-    """
-    :param text:上一步获取的xml的text格式
-    :return: 弹幕列表
-    """
-    logger.debug("弹幕数据处理中")
-    soup = bs4.BeautifulSoup(text, "html.parser")
-    # 获取d标签内的内容，并提取出来
-    barrage_list = []
-    for i in soup.findAll(name='d'):
-        barrage_list.append(i.text)
-    logger.debug("处理完毕")
-    return barrage_list[1:]
-
-
-# 存储并处理爬取到的弹幕
-def data_preprocess(barrage, file_name):
-    """
-    :param barrage:爬取到的弹幕列表，转化为dict后交给pandas转化为csv
-    :param file_name: 为存储文件取名
-    """
-    data_dict = {
-        'barrage': barrage
+def save_comment(comment, file_name):
+    comment_dict = {
+        'comment': comment
     }
-    pd_data = pd.DataFrame(data_dict)
+    pd_data = pd.DataFrame(comment_dict)
     try:
-        logger.info(f"向{file_name}.csv写入弹幕文件")
-        pd_data.to_csv(f'../data/csv/{file_name}.csv', index=False, header=False, mode='w', encoding="utf-8-sig")
+        logger.info("向{}_评论.csv中写入评论".format(file_name))
+        pd_data.to_csv(f'../data/csv/{file_name}_评论.csv', index=False, header=False, mode='w', encoding="utf-8-sig")
     except Exception as e:
         logger.error(f"{file_name}.csv写入过程发生错误,错误原因: {e}")
     try:
-        logger.info(f"对{file_name}.csv内的弹幕进行处理")
-        with open(f"../data/csv/{file_name}.csv", mode='r', encoding="utf-8") as f:
+        logger.info(f"对{file_name}_评论.csv内的评论进行处理")
+        with open(f"../data/csv/{file_name}_评论.csv", mode='r', encoding="utf-8") as f:
             reader = f.read().replace('\n', '')
             # 加载停用词表
             stopwords = [line.strip() for line in open('../data/stop_words.txt', encoding='utf-8').readlines()]
@@ -147,7 +149,61 @@ def data_preprocess(barrage, file_name):
                 sentence = sentence
         return sentence
     except Exception as e:
-        logger.error("发生错误:{}".format(e))
+        logger.error("处理评论时发生错误:{}".format(e))
+
+
+# 处理爬取结果
+def parse_html(text) -> list:
+    """
+    :param text:上一步获取的xml的text格式
+    :return: 弹幕列表
+    """
+    logger.debug("弹幕数据处理中")
+    soup = bs4.BeautifulSoup(text, "html.parser")
+    # 获取d标签内的内容，并提取出来
+    barrage_list = []
+    for i in soup.findAll(name='d'):
+        barrage_list.append(i.text)
+    logger.debug("弹幕处理完毕")
+    return barrage_list[1:]
+
+
+# 存储并处理爬取到的弹幕
+def data_preprocess(barrage, file_name):
+    """
+    :param barrage:爬取到的弹幕列表，转化为dict后交给pandas转化为csv
+    :param file_name: 为存储文件取名
+    """
+    data_dict = {
+        'barrage': barrage
+    }
+    pd_data = pd.DataFrame(data_dict)
+    try:
+        logger.info(f"向{file_name}_弹幕.csv写入弹幕文件")
+        pd_data.to_csv(f'../data/csv/{file_name}_弹幕.csv', index=False, header=False, mode='w', encoding="utf-8-sig")
+    except Exception as e:
+        logger.error(f"{file_name}_弹幕.csv写入过程发生错误,错误原因: {e}")
+    try:
+        logger.info(f"对{file_name}_弹幕.csv内的弹幕进行处理")
+        with open(f"../data/csv/{file_name}_弹幕.csv", mode='r', encoding="utf-8") as f:
+            reader = f.read().replace('\n', '')
+            # 加载停用词表
+            stopwords = [line.strip() for line in open('../data/stop_words.txt', encoding='utf-8').readlines()]
+            # 去标点，去空白，去数字
+            logger.debug("去除标点符号、空白符、数字")
+            pun_num = string.punctuation + string.digits
+            table = str.maketrans('', '', pun_num)
+            reader = reader.translate(table)
+            seg_list = jieba.lcut(reader, cut_all=True)  # 精确分词
+            sentence = ''
+            for word in seg_list:
+                if word not in stopwords and word.isspace() is False:
+                    sentence += word
+                    sentence += ","
+                sentence = sentence
+        return sentence
+    except Exception as e:
+        logger.error("处理弹幕时发生错误:{}".format(e))
 
 
 # 记录词频
@@ -162,7 +218,7 @@ def count_words(text, file_name):
     for word in words:
         frequency[word] = frequency.get(word, 0) + 1
     pd_count = pd.DataFrame(frequency, index=['times']).T.sort_values('times', ascending=False)
-    pd_count.to_csv(f'../data/csv/{file_name}_words_frequency.csv')
+    pd_count.to_csv(f'../data/csv/{file_name}_词频.csv', mode='a', header=None)
 
 
 # 生成词云
@@ -185,6 +241,10 @@ def gen_cwd(text, file_name):
 if __name__ == '__main__':
     video_url = str(input("请输入需要爬取的视频地址:"))
     barrage_data = get_barrage(video_url)
+    vd_aid = get_av(video_url)
+    comments = get_comments(vd_aid)
+    comment_sentence = save_comment(comments, NAME)
+    count_words(comment_sentence, NAME)
     p_data = parse_html(barrage_data)
     my_sentence = data_preprocess(p_data, NAME)
     count_words(my_sentence, NAME)
--- common/test.py
+++ common/test.py
@@ -1,33 +1,23 @@
-import re
-import math
+import threading
+import threadpool
+import requests
 
-import requests as requests
-from jsonpath import jsonpath
-import bs4
+pool = threadpool.ThreadPool(10)
+lock = threading.Lock()
 
-# url = "https://www.bilibili.com/video/BV1t5411K79e?spm_id_from=333.5.b_67616d655f7374616e645f616c6f6e65.39"
-# url2 = "https://api.bilibili.com/x/v2/reply?pn=1&type=1&oid=930710044&sort=1"
-# res = requests.get(url).text
-# res2 = requests.get(url2).json()
-# aid = re.search(r"(?<=aid\":)\d*", res).group()
-# p = r"(?<=/video/).{12}"
-# bvid = re.search(pattern=p, string=url).group()
-# cid_url = "https://api.bilibili.com/x/player/pagelist?bvid={}&jsonp=jsonp".format(bvid)
-# res = requests.get(cid_url).json()
-# cid = jsonpath(res, "$..data[0]")[0]['cid']
+aList = []
+
+
+def get_data(num):
+    res = requests.get("https://api.bilibili.com/x/v2/reply?pn=1&type=1&oid=455513434&sort=1").json()
+    lock.acquire()
+    aList.extend(res)
+    lock.release()
+    print(aList)
+
 
 if __name__ == '__main__':
-    # print(res)
-    # print(aid)
-    # print(cid)
-    # c = jsonpath(res2, "$..data[page].acount")[0]
-    # print(c)
-    res = requests.get(f"https://api.bilibili.com/x/v2/reply?pn=1&type=1&oid=459511646&sort=1").json()
-    comment_list = []
-    comment_count = jsonpath(res, "$..data[page].count")[0]
-    for i in range(math.ceil(comment_count/20)):
-        res = requests.get(f"https://api.bilibili.com/x/v2/reply?pn={i + 1}&type=1&oid=459511646&sort=1").json()
-        comment = jsonpath(res, r"$..data[replies]..[content][message]")
-        comment_list.extend(comment)
-    # print("{}haha".format(math.ceil(comment_count/20) + 1))
-    print(comment_list)
+    li = [i for i in range(1, 11)]
+    tasks = threadpool.makeRequests(get_data, li)
+    [pool.putRequest(task) for task in tasks]
+    pool.wait()
